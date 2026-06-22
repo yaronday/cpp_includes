48f83420caf4d32b3235780ca8ee2272c2b8ae2b
yaronday <yaronday77@gmail.com>
2026-06-22 09:59:55 +0300

    linting and formatting

--- byte_swapper/byteswapper_test.cpp
+++ byte_swapper/byteswapper_test.cpp
@@ -19,7 +19,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-*/
+ */
 #include "byteswapper.hpp"
 
 // --- Unit Tests for byteswapper (endianity swap) ---
@@ -35,24 +35,25 @@ static_assert(byteswapper(static_cast<uint16_t>(0x1234)) == 0x3412, "16-bit stan
 static_assert(byteswapper(static_cast<uint16_t>(0xFFFF)) == 0xFFFF, "16-bit max failed");
 
 // 32-bit tests
-static constexpr uint32_t allZeros32 = 0x00000000; 
-static constexpr uint32_t allOnes32 =  0xFFFFFFFF;
+static constexpr uint32_t allZeros32 = 0x00000000;
+static constexpr uint32_t allOnes32 = 0xFFFFFFFF;
 static_assert(byteswapper(static_cast<uint32_t>(allZeros32)) == allZeros32, "32-bit zero failed");
 static_assert(byteswapper(static_cast<uint32_t>(0x12345678)) == 0x78563412, "32-bit standard failed");
 static_assert(byteswapper(static_cast<uint32_t>(allOnes32)) == allOnes32, "32-bit max failed");
 
 // 64-bit tests
-static constexpr uint64_t allZeros64 = 0x0000000000000000ULL; 
-static constexpr uint64_t allOnes64 =  0xFFFFFFFFFFFFFFFFULL;
+static constexpr uint64_t allZeros64 = 0x0000000000000000ULL;
+static constexpr uint64_t allOnes64 = 0xFFFFFFFFFFFFFFFFULL;
 
 static constexpr uint64_t original = 0x123456789ABCDEF0ULL;
 static constexpr uint64_t first = byteswapper(original);
 static constexpr uint64_t second = byteswapper(first);
 static_assert(byteswapper(static_cast<uint64_t>(allZeros64)) == allZeros64, "64-bit zero failed");
 static_assert(first == 0xF0DEBC9A78563412ULL, "64-bit standard failed");
-static_assert(second == original, "64-bit standard failed"); 
+static_assert(second == original, "64-bit standard failed");
 static_assert(byteswapper(static_cast<uint64_t>(allOnes64)) == allOnes64, "64-bit max failed");
 
-int main() {
+int main()
+{
     return 0;
 }
